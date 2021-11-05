e72f620b7114bbf2af947ad9177a3c53d2f2aa39
magus <harvestashya@gmail.com>
2021-11-05 16:22:48 -0700

    edit header

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -12,7 +12,7 @@ import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from 'containers/HomePage/Loadable';
-import FeaturePage from 'containers/FeaturePage/Loadable';
+// import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
@@ -31,16 +31,13 @@ const AppWrapper = styled.div`
 export default function App() {
   return (
     <AppWrapper>
-      <Helmet
-        titleTemplate="%s - React.js Boilerplate"
-        defaultTitle="React.js Boilerplate"
-      >
-        <meta name="description" content="A React.js Boilerplate application" />
+      <Helmet titleTemplate="%s - DMI takehome" defaultTitle="DMI takehome">
+        <meta name="description" content="The takehome assignment for DMI" />
       </Helmet>
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
+        {/* <Route path="/features" component={FeaturePage} /> */}
         <Route path="" component={NotFoundPage} />
       </Switch>
       <Footer />
